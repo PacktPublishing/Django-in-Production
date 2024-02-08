95540f489aac54e60530d5ab4a4f3306ce9abbc9
argo <arghyasaha26@gmail.com>
2024-02-08 23:46:30 +0530

    Fixed chapter 4 code issue

--- Chapter04/myblog/backend/author/models.py
+++ Chapter04/myblog/backend/author/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    # demo_field = models.TextField(default='demo') # Uncomment this line to test fake migrations
     email = models.EmailField(unique=True)
     bio = models.TextField()
 
